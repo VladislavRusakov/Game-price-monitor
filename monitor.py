17dfd88eb454df663b539b13924c60b04edf53a1
Vladislav Rusakov <v.rusakov@q-pol.ru>
2021-11-01 15:18:08 +0300

    Version 2.0

---
+++ monitor.py
@@ -0,0 +1,70 @@
+import requests
+import sys
+import re
+
+from bs4 import BeautifulSoup as BS
+
+
+def request_caller_for_lists(data: list[str]) -> None:
+    """Вызывает функцию обращения по для каждой ссылки списка"""
+    for url in data:
+        get_data_from_hotgame(url)
+
+
+def get_name(mode: str = "input") -> None:
+    """Вызывает функции создания url в зависимости от режима
+    Режим input предлагает ввод названия через консоль
+    Режим text читает названия из файла games.txt"""
+    if mode == "input":
+        print("Введите название")
+        name = input()
+        make_an_url(name)
+    elif mode == "text":
+        with open('games.txt', 'r') as file:
+            data = file.read().splitlines()
+        request_caller_for_lists(data)
+    else:
+        print("Incorrect mode")
+
+
+def make_an_url(name: str) -> str:
+    """Создаёт корректную ссылку на хотгейм"""
+    names = name.split(" ")
+    for index, _ in enumerate(names):
+        names[index] = _.capitalize()
+    url = f"https://hot-game.info/game/{'-'.join(names)}"
+    print(url)
+    get_data_from_hotgame(url)
+
+
+def mode_selector():
+    """Вызывает get_name, передавая туда название
+    режима из консоли. Если режима нет, просто вызывает
+    get_name()
+    Режимы: text и input"""
+    try:
+        mode = sys.argv[1]
+        get_name(mode)
+    except IndexError:
+        get_name()
+
+
+def get_data_from_hotgame(url: str) -> None:
+    """Обращается к сайту hot-games.info
+    Парсит HTML, находит нужную строчку с ценой
+    Печатает её в консоль
+    """
+    print(url, end=" - ")
+    response = requests.get(url)
+    if str(response.status_code) == "404":
+        print("Game not found")
+
+    elif str(response.status_code) == "200":
+        soup = BS(response.text, "html.parser")
+        data = soup.findAll('span', class_="price-value")
+        pattern = (r'data-final_price="(\d)*"')
+        result = re.search(pattern, str(data[0]))
+        print(result.group(0))
+
+
+mode_selector()
